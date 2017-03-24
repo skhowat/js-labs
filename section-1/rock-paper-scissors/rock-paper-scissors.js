9f591fc37e9d2cab2a4e6226c97c46d87338feb7
Stevie Howat <skhowat@gmail.com>
2017-03-24 10:37:08 -0500

    finished rock-paper-scissors

--- section-1/rock-paper-scissors/rock-paper-scissors.js
+++ section-1/rock-paper-scissors/rock-paper-scissors.js
@@ -1,7 +1,31 @@
 
 function whoWins(first, second) {
-  if (first === 'rock') && (second === 'scissors') {
+  if (first === 'rock' && second === 'scissors') {
     return 'first';
+  }
+
+  if  (first === 'scissors' && second === 'rock') {
+    return 'second';
+  }
+
+  if (first === 'paper' && second === 'rock') {
+    return 'first';
+  }
+
+  if (first === 'rock' && second === 'paper') {
+    return 'second';
+  }
+
+  if (first === 'scissors' && second === 'paper') {
+    return 'first';
+  }
+
+  if (first === 'paper' && second === 'scissors') {
+    return 'second';
+  }
+
+  if (first === second) {
+    return 'tie';
   }
 
 
